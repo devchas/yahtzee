f4dfdf22f7dc39a96edacf14c1012d43b6179492
S <s>
2016-02-17 16:38:39 -0500

    {"types":"C","runs":27}

--- Yahtzee.java
+++ Yahtzee.java
@@ -85,7 +85,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
  */
 	private void enactTurn(int plyr) {
 		int scCat;
-		display.printMessage(playerNames[plyr] + PLYR_ROLL_MSG);
+		display.printMessage(playerNames[plyr - 1] + PLYR_ROLL_MSG);
 		display.waitForPlayerToClickRoll(plyr);
 		// Loops through all of player's rolls
 		for (int i = 0; i < MAX_ROLLS; i++) {
