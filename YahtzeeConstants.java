cd4df9a4c56a4a14dc866c28c44e53d419ea3302
S <s>
2016-02-17 08:54:44 -0500

    {"types":"C","runs":0}

--- YahtzeeConstants.java
+++ YahtzeeConstants.java
@@ -43,5 +43,28 @@ public interface YahtzeeConstants {
 	public static final int CHANCE = 15;
 	public static final int LOWER_SCORE = 16;
 	public static final int TOTAL = 17;
+	
+/** The lowest and highest dice roll */
+	public static final int DIE_LOW = 1;
+	public static final int DIE_HIGH = 6;
+	
+/** Maximum rolls per player */
+	public static final int MAX_ROLLS = 3;
+	
+/** The points and point multiples rewarded for different rolls */
+	public static final int TRIPS_MULTIPLE = 3;
+	public static final int QUADS_MULTIPLE = 4;
+	public static final int FULL_HOUSE_PTS = 25;
+	public static final int SM_STAIGHT_PTS = 30;
+	public static final int LG_STRAIGHT_PTS = 40;
+	public static final int YAHTZEE_PTS = 50;
+	public static final int UP_BONUS_THRESH = 63;
+	public static final int UP_BONUS_PTS = 35;
+	
+/** Messages displayed during game */
+	public static final String PLYR_ROLL_MSG = " turn. Click \"Roll Dice\" button to roll the dice.";
+	public static final String RE_ROLL_MSG = "Select the dice you wish to re-roll and click \"Roll Again\".";
+	public static final String SEL_CAT_MSG = "Select a category for this roll.";
+	public static final String SEL_DIF_CAT_MSG = "That category has already been used. Select a different category.";
   
 }
