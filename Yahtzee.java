3f164c493f1a332c88aa6d7b82be1bcd755324b2
S <s>
2016-02-17 09:13:55 -0500

    {"types":"C","runs":0}

--- Yahtzee.java
+++ Yahtzee.java
@@ -26,7 +26,41 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	}
 
 	private void playGame() {
-		/* You fill this in */
+		int turn = 0;
+		while(true) {
+			turn++;
+			if (turn > N_SCORING_CATEGORIES) break;
+			for (int i = 0; i < nPlayers; i++) {
+				enactTurn(i);
+			}
+			calcFinalScores();
+			endGame();
+		}
+	}
+	
+/**
+ * Processes a players entire turn
+ * @param plyr Player number
+ */
+	private void enactTurn(int plyr) {
+		display.printMessage(playerNames[plyr] + PLYR_ROLL_MSG);
+		// Loops through all of player's rolls
+		for (int i = 0; i < MAX_ROLLS; i++) {
+			display.waitForPlayerToClickRoll(plyr);
+			display.displayDice(rollDice());
+			display.waitForPlayerToClickRoll(plyr);
+			display.printMessage(RE_ROLL_MSG);
+		}
+		// Gets player's scoring category and checks availability
+		display.printMessage(SEL_CAT_MSG);
+		while(true) {
+			// Subtract 1 to align response with array
+			int scCat = display.waitForPlayerToSelectCategory() - 1;
+			if (catAvail(plyr, scCat)) break;
+			display.printMessage(SEL_DIF_CAT_MSG);
+		}
+		display.updateScorecard(scCat, plyr, calcScore(scCat));
+		
 	}
 		
 /* Private instance variables */
@@ -34,5 +68,6 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	private String[] playerNames;
 	private YahtzeeDisplay display;
 	private RandomGenerator rgen = new RandomGenerator();
-
+	private int[] dice = new int[N_DICE];
+	private int[] scCats = new int[N_SCORING_CATEGORIES];
 }
