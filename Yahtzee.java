ccf80dda4e29c6108749d4df72371ea6984437b5
S <s>
2016-02-18 15:54:19 -0500

    {"types":"C","runs":8}

--- Yahtzee.java
+++ Yahtzee.java
@@ -315,7 +315,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	private void endGame() {
 		int winner = 0;
 		for (int i = 0; i < nPlayers; i++) {
-			if (upBonus[i] == 0) display.updateScorecard(UPPER_BONUS, i, 0);
+			if (upBonus[i] == 0) display.updateScorecard(UPPER_BONUS, i + 1, 0);
 		}
 		for (int i = 1; i < nPlayers; i++) {
 			if (totScore[i] > totScore[winner]) winner = i;
