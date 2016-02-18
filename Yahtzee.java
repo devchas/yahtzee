10eaecb47e25acdaed4c9e5adaba4cea8e8265a3
S <s>
2016-02-18 08:59:25 -0500

    {"types":"C","runs":11}

--- Yahtzee.java
+++ Yahtzee.java
@@ -220,8 +220,8 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
  * @return True if there are n dice with the same number, else false
  */
 	private boolean checkExact(int n) {
-		int cnt = 1;
 		for (int i = 0; i < N_DICE - n + 1; i++) {
+			int cnt = 1;
 			for (int j = i + 1; j < N_DICE; j++) {
 				if (dice[i] ==  dice[j]) cnt++;
 			}
